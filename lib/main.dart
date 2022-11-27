3f005eabd9b9e041f388ba9ea0a1becb374d14c9
fielaazhari <azharijunita@gmail.com>
2022-11-27 13:56:40 +0700

    Menambahkan page/profile

--- lib/main.dart
+++ lib/main.dart
@@ -18,7 +18,7 @@ class MyApp extends StatelessWidget {
         useMaterial3: true,
         fontFamily: 'Rubik',
       ),
-      home: const ProfileScreen(),
+      home: const EditProfileScreen(),
     );
   }
 }
