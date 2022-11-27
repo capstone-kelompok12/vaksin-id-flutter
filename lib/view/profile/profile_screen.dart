4f403283430836422dbeb6ff5201d3cc8c19c730
fielaazhari <azharijunita@gmail.com>
2022-11-27 13:46:27 +0700

    Menambahkan page/profile

--- lib/view/profile/profile_screen.dart
+++ lib/view/profile/profile_screen.dart
@@ -19,28 +19,38 @@ class ProfileScreen extends StatelessWidget {
       body: Column(
         crossAxisAlignment: CrossAxisAlignment.stretch,
         children: [
-          SizedBox(
-            child: DecoratedBox(
-              decoration: const BoxDecoration(
-                color: Color(0xFFE7F1E8),
-              ),
-              child: Padding(
-                padding: const EdgeInsets.all(10),
-                child: Column(
-                  children: const [
-                    Icon(
-                      Icons.account_circle_outlined,
-                      size: 76.67,
-                    ),
-                    Text(
-                      'Nama Lengkap',
-                      style:
-                          TextStyle(fontSize: 16, fontWeight: FontWeight.w400),
-                    ),
-                    Text('username@gmail.com',
+          Padding(
+            padding: const EdgeInsets.all(2),
+            child: SizedBox(
+              width: 360,
+              height: 174,
+              child: DecoratedBox(
+                decoration: const BoxDecoration(
+                  color: Color(0xFFE7F1E8),
+                  borderRadius: BorderRadius.all(Radius.circular(10)),
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.all(10),
+                  child: Column(
+                    children: const [
+                      Padding(
+                        padding: EdgeInsets.all(8.0),
+                        child: Icon(
+                          Icons.account_circle_outlined,
+                          size: 76.67,
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 10,
+                      ),
+                      Text('Nama Lengkap',
                         style: TextStyle(
-                            fontSize: 16, fontWeight: FontWeight.w400))
-                  ],
+                              fontSize: 16,fontWeight: FontWeight.w400,),),
+                      Text('username@gmail.com',
+                          style: TextStyle(
+                              fontSize: 16, fontWeight: FontWeight.w400),),
+                    ],
+                  ),
                 ),
               ),
             ),
@@ -81,35 +91,40 @@ class ProfileScreen extends StatelessWidget {
                   color: Colors.black26,
                   thickness: 1,
                 ),
-                ListTile(
-                  leading: Icon(
-                    Icons.groups_outlined,
-                    color: Colors.black,
-                    size: 20,
-                  ),
-                  title: Text('Anggota Keluarga',
-                      style:
-                          TextStyle(fontSize: 14, fontWeight: FontWeight.w400)),
-                  trailing:
-                      Icon(Icons.arrow_right, color: Colors.black, size: 20),
-                ),
-                Divider(
-                  color: Colors.black87,
-                  thickness: 1,
-                ),
+                // ListTile(
+                //   leading: Icon(
+                //     Icons.groups_outlined,
+                //     color: Colors.black,
+                //     size: 20,
+                //   ),
+                //   title: Text('Anggota Keluarga',
+                //       style:
+                //           TextStyle(fontSize: 14, fontWeight: FontWeight.w400)),
+                //   trailing:
+                //       Icon(Icons.arrow_right, color: Colors.black, size: 20),
+                // ),
+                // Divider(
+                //   color: Colors.black87,
+                //   thickness: 1,
+                // ),
               ],
             ),
-          )
+          ),
         ],
       ),
-      bottomNavigationBar: BottomNavigationBar(
-        type: BottomNavigationBarType.fixed,
-        items: const <BottomNavigationBarItem>[
-          BottomNavigationBarItem(icon: Icon(Icons.circle), label: 'Label'),
-          BottomNavigationBarItem(icon: Icon(Icons.circle), label: 'Label'),
-          BottomNavigationBarItem(icon: Icon(Icons.circle), label: 'Label'),
-          BottomNavigationBarItem(icon: Icon(Icons.circle), label: 'Label'),
-        ],
+      bottomNavigationBar: SizedBox(
+        height: 80,
+        width: 360,
+        child: BottomNavigationBar(
+          backgroundColor: Color(0xFFE7F1E8),
+          type: BottomNavigationBarType.fixed,
+          items: const <BottomNavigationBarItem>[
+            BottomNavigationBarItem(icon: Icon(Icons.circle), label: 'Label'),
+            BottomNavigationBarItem(icon: Icon(Icons.circle), label: 'Label'),
+            BottomNavigationBarItem(icon: Icon(Icons.circle), label: 'Label'),
+            BottomNavigationBarItem(icon: Icon(Icons.circle), label: 'Label'),
+          ],
+        ),
       ),
     );
   }
