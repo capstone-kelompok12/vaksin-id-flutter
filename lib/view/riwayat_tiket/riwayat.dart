645ddd2c9f94aa8a8357370d6c50ccc66d0db058
NdaruDienul <dienuln@gmail.com>
2022-12-09 18:22:46 +0700

    menambahkan page riwayat

--- lib/view/riwayat_tiket/riwayat.dart
+++ lib/view/riwayat_tiket/riwayat.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
+import 'package:vaksin_id_flutter/view/riwayat_tiket/tiket_vaksin.dart';
 
 class RiwayatPage extends StatefulWidget {
   const RiwayatPage({super.key});
@@ -9,12 +10,34 @@ class RiwayatPage extends StatefulWidget {
   State<RiwayatPage> createState() => _RiwayatPageState();
 }
 
-class _RiwayatPageState extends State<RiwayatPage> {
+class _RiwayatPageState extends State<RiwayatPage>
+    with TickerProviderStateMixin {
+  TabController? tabController;
+  @override
+  void initState() {
+    tabController = TabController(length: 3, vsync: this);
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        child: const Center(child: Text('Riwayat')),
+      appBar: AppBar(
+        title: Text("Tiket Vaksinasi"),
+        bottom: TabBar(
+          controller: tabController,
+          tabs: const <Widget>[
+            Tab(
+              child: Text("Tiket Vaksin"),
+            ),
+            Tab(
+              child: Text("Riwayat"),
+            ),
+          ],
+        ),
+      ),
+      body: const TabBarView(
+        children: <Widget>[TiketVaksinPage(), RiwayatPage()],
       ),
     );
   }
